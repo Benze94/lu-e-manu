8fb684588cc3879b2240e04cf32260d7a0156efc
Emanuele Rovaris <emanuelerovaris@gmail.com>
2022-10-30 16:05:15 +0100

    feat: first homepage

---
+++ src/pages/homepage/Homepage.tsx
@@ -0,0 +1,18 @@
+import './Homepage.scss';
+
+const Homepage = () => {
+  return (
+    <div className="Homepage">
+      <div className='Homepage__saveTheDate'>
+        <div className='Homepage__title'>
+          <div className='Homepage__titleWord'>Save</div>
+          <div className='Homepage__titleWord'>the</div>
+          <div className='Homepage__titleWord'>date</div>
+        </div>
+        <div className='Homepage__date'>30 Settembre 2023</div>
+      </div>
+    </div>
+  )
+}
+
+export default Homepage;
