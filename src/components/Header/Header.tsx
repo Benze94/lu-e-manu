f7c70b7cc71957942425426489b46de399eb23f7
Emanuele Rovaris <emanuelerovaris@gmail.com>
2023-05-18 17:27:29 +0200

    feat: logo

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import './Header.scss'
+
+const Header = (): JSX.Element => {
+  return (
+    <div className="Header">
+      <div className="Header__logo"></div>
+    </div>
+  )
+}
+
+export default Header
--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -1,10 +1,12 @@
 import Navbar from '../../components/Navbar/Navbar'
 import MainPicture from '../../components/MainPicture/MainPicture'
 import './Homepage.scss'
+import Header from '../../components/Header/Header'
 
 const Homepage = () => {
   return (
     <div className="Homepage">
+      <Header />
       <Navbar />
       <MainPicture />
     </div>
